93306d89d64475f0da305eb8b00e2eee3a842377
abishekGithub <abishekshah12315@gmail.com>
2022-06-14 00:15:30 +0545

    committ

--- src/components/Project.js
+++ src/components/Project.js
@@ -37,9 +37,7 @@ const Project = () =>{
     <div className="cart">
         <div className="title_decription">
             <p className="title">GitHub Api</p>
-            <p className="description">This project is all about my 
-            Website. where anyone can visit and know about me,
-            also can see my project's as well as contact me.
+            <p className="description">created app where i have fetched the github users from the api 
             </p>
         </div>
         <div className="icons_visit">
@@ -50,6 +48,46 @@ const Project = () =>{
             </div>
             <div className="visit">
                 <button><a href="https://githubapiabishek.netlify.app" target="_blank">Visit Project</a></button>
+            </div>
+    </div>
+    </div>
+
+    <div className="cart">
+        <div className="title_decription">
+            <p className="title">FoodRecipe App</p>
+            <p className="description"> created the simple api where i have fetched all the 
+            category from the api. here after visiting the navbar we can find the different items in the 
+            different category. 
+            </p>
+        </div>
+        <div className="icons_visit">
+            <div className="icons">
+            <p className="icon"><FaHtml5/></p>
+            <p className="icon"><FaCss3/></p>
+            <p className="icon"><FaReact/></p>
+            </div>
+            <div className="visit">
+                <button><a href="https://foodrecipeapishah.netlify.app" target="_blank">Visit Project</a></button>
+            </div>
+    </div>
+    </div>
+
+    <div className="cart">
+        <div className="title_decription">
+            <p className="title">FoodRecipe App</p>
+            <p className="description"> created the simple api where i have fetched all the 
+            category from the api. here after visiting the navbar we can find the different items in the 
+            different category. 
+            </p>
+        </div>
+        <div className="icons_visit">
+            <div className="icons">
+            <p className="icon"><FaHtml5/></p>
+            <p className="icon"><FaCss3/></p>
+            <p className="icon"><FaReact/></p>
+            </div>
+            <div className="visit">
+                <button><a href="https://foodrecipeapishah.netlify.app" target="_blank">Visit Project</a></button>
             </div>
     </div>
     </div>
